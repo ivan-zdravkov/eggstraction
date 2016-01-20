636b500a80e1a3a66000a11e2fea135fb38d4685
Ivan Zdravkov <IvanZDravkovBG@gmail.com>
2016-01-20 20:11:19 +0200

    Basic Character, Camera and Sky movement relative to one another.

---
+++ Assets/Classes/PositionTreshholds.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Classes
+{
+    public class PositionTreshholds
+    {
+        private float horizontalTreshhold;
+        private float verticalTreshhold;
+
+        private float upperTreshhold;
+        private float lowerTreshhold;
+        private float leftTreshhold;
+        private float rightTreshhold;
+
+        public float HorizontalTreshhold
+        {
+            get
+            {
+                return this.horizontalTreshhold;
+            }
+        }
+
+        public float VerticalTreshhold
+        {
+            get
+            {
+                return this.verticalTreshhold;
+            }
+        }
+
+        public float UpperTreshhold
+        {
+            get
+            {
+                return this.upperTreshhold;
+            }
+        }
+
+        public float LowerTreshhold
+        {
+            get
+            {
+                return this.lowerTreshhold;
+            }
+        }
+
+        public float LeftTreshhold
+        {
+            get
+            {
+                return this.leftTreshhold;
+            }
+        }
+
+        public float RightTreshhold
+        {
+            get
+            {
+                return this.rightTreshhold;
+            }
+        }
+
+        public PositionTreshholds(int horizontalTreshholdPercentage, int verticalTreshholdPercentage)
+        {
+            this.horizontalTreshhold = horizontalTreshholdPercentage / 100.0f;
+            this.verticalTreshhold = verticalTreshholdPercentage / 100.0f;
+        }
+
+        public void UpdateTreshholds (float upper, float lower, float left, float right)
+        {
+            this.upperTreshhold = upper;
+            this.lowerTreshhold = lower;
+            this.leftTreshhold = left;
+            this.rightTreshhold = right;
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Classes;
+using System;
+
+public class CharacterController : MonoBehaviour {
+    private Camera camera;
+    private GameObject sky;
+
+    private PositionTreshholds positionTreshholds;
+
+    private const float moveSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+        this.camera = Camera.main;
+        this.sky = GameObject.Find("Sky");
+
+        positionTreshholds = new PositionTreshholds(25, 25);
+
+        this.UpdatePositionThresholds();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        #region MovePlayer
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 moveUpVector = new Vector3(0, moveSpeed);
+
+            transform.Translate(moveUpVector);
+
+            if (transform.position.y > positionTreshholds.UpperTreshhold)
+            {
+                this.MoveEnvironment(moveUpVector);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 moveDownVector = new Vector3(0, -moveSpeed);
+
+            transform.Translate(moveDownVector);
+
+            if (transform.position.y < positionTreshholds.LowerTreshhold)
+            {
+                this.MoveEnvironment(moveDownVector);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 moveRightVector = new Vector3(moveSpeed, 0);
+
+            transform.Translate(moveRightVector);
+
+            if (transform.position.x > positionTreshholds.RightTreshhold)
+            {
+                this.MoveEnvironment(moveRightVector);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 moveLeftVector = new Vector3(-moveSpeed, 0);
+
+            transform.Translate(moveLeftVector);
+
+            if (transform.position.x < positionTreshholds.LeftTreshhold)
+            {
+                this.MoveEnvironment(moveLeftVector);
+            }
+        }
+        #endregion
+    }
+
+    private void MoveEnvironment(Vector3 moveVector)
+    {
+        camera.transform.Translate(moveVector);
+        sky.transform.Translate(moveVector);
+
+        this.UpdatePositionThresholds();
+    }
+
+    private void UpdatePositionThresholds()
+    {
+        float height = 2f * this.camera.orthographicSize;
+        float width = height * this.camera.aspect;
+
+        float upper = this.camera.transform.position.y + (height * positionTreshholds.VerticalTreshhold);
+        float lower = this.camera.transform.position.y - (height * positionTreshholds.VerticalTreshhold);
+        float left = this.camera.transform.position.x - (width * positionTreshholds.HorizontalTreshhold);
+        float right = this.camera.transform.position.x + (width * positionTreshholds.HorizontalTreshhold);
+
+        positionTreshholds.UpdateTreshholds(upper, lower, left, right);
+    }
+}
